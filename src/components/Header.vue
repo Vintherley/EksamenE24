35c2b7643e4f69d68db05a09af839dcd84d559dc
tanja <tanjamusen1@gmail.com>
2024-11-12 11:16:52 +0100

    Adding Basket paige. finish the navbar. still making some small changes so it fit with the other componet and paiges

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,11 +11,16 @@ const toggleMenu = () => {
 <nav class="menu">
   <RouterLink to="/FrontPaige.vue">
     <img src="@/assets/img/logo.png" alt="Logo" class="logo" /></RouterLink>
+    <div class="basket-links">
+    <RouterLink to="/Basket.vue">
+        <img src="@/assets/img/basketimg.png" alt="basket" class="basket" /></RouterLink>
   <button class="burger-menu" @click="toggleMenu">☰</button> <!-- Burger-menu-knap -->
   <div :class="{ 'nav-links': true, 'active': isMenuOpen }">
+    <RouterLink to="/" class="nav-link">Forside</RouterLink>
     <RouterLink to="/AboutOS.vue" class="nav-link">Om Os</RouterLink>
     <RouterLink to="/webshop.vue" class="nav-link">Shop</RouterLink>
   </div>
+</div>
 </nav>
 </template>
 
@@ -36,9 +41,18 @@ const toggleMenu = () => {
     background-color: #194011;
     width: 100vh;
     height: 68px;
+    padding: 20px;
+    margin: 0px 10px 5px -10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    
+    
 }
   .burger-menu {
-    display: block;                /* Vis burger-menuen på mobil */
+    display: block;   
+    background-color: #194011;           /* Vis burger-menuen på mobil */
+    
   }
 
   .nav-links {
@@ -46,21 +60,42 @@ const toggleMenu = () => {
     flex-direction: column;
     align-items: center;
     background-color: #194011;
-    width: 100%;
+    width: auto;
     padding: 10px 0;
   }
 
   .nav-links.active {
-    display: flex;                 /* Vis links, når burger-menuen er åben */
+    position: absolute;
+    display: flex;
+    right: 0;
+    margin-top: 200px;
+    width: 135px;
+    z-index: 5;
+                    /* Vis links, når burger-menuen er åben */
   }
 
   .nav-link {
     margin: 10px 0;                /* Plads mellem links */
     font-size: 18px;
+    color:#EDEAD0; 
+    text-decoration: none;
+    
   }
   .logo{
     width: 61px;
     height: 70px;
+  }
+  .basket{
+    display:flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    margin: 10px;
+  }
+  .basket-links{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin: 10px;
   }
 }
 }
